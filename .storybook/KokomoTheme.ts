dba7b8741f1182c3d97f875a6e453a10fc53e553
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-09-17 12:14:04 -0500

    feat: adds KokomoTheme.ts and manager.ts to storybook

---
+++ .storybook/KokomoTheme.ts
@@ -0,0 +1,9 @@
+import { create } from "storybook/theming";
+
+export default create({
+  base: "dark",
+  brandTitle: "Kokomo UI Storybook",
+  brandUrl: "https://github.com/andrewgilliland/kokomo-ui",
+  brandImage: "../src/assets/pixel-palm-tree-2.jpg",
+  brandTarget: "_self",
+});
---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from "storybook/manager-api";
+import kokomoTheme from "./KokomoTheme";
+
+addons.setConfig({
+  theme: kokomoTheme,
+});
